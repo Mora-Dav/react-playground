d4b3c0cfcc13a6429ff3d4c54681953e07dca11c
Mora-Dav <moradavi@gmail.com>
2021-02-06 08:58:21 -0300

    Primera app con React -Peliculas

---
+++ src/components/Pelicula.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+function Pelicula() {
+    return (
+        <>
+            <h2>Titulo de la Pelicula !!!</h2>
+            <p>Rating</p>
+        </>
+    )
+}
+
+
+
+export default Pelicula ;
---
+++ src/components/TiraPeliculas.js
@@ -0,0 +1,15 @@
+import Pelicula from './Pelicula';
+
+function TiraPeliculas() {
+    return (
+        <div>
+            <Pelicula />
+            <Pelicula />
+            <Pelicula />
+            <Pelicula />
+            <Pelicula />
+        </div>
+    )
+}
+
+export default TiraPeliculas
