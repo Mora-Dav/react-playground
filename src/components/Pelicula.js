947e16e3e256dbaa41826677eee59cb2f09d6c7f
Mora-Dav <moradavi@gmail.com>
2021-02-06 09:53:40 -0300

    JSX

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -1,10 +1,17 @@
 import React from 'react' ;
 
 function Pelicula() {
+
+    let rating = 5.8;
+    let generos = ["Accion", "Drama"]
+
     return (
         <>
             <h2>Titulo de la Pelicula !!!</h2>
-            <p>Rating</p>
+            <p>Rating: {rating} </p>
+            <ul>
+                {generos.map( genero => <li> {genero} </li>)}
+            </ul>
         </>
     )
 }
