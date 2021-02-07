26e899408db1ac437a4274785e1a9f92dcfdbb28
Mora-Dav <moradavi@gmail.com>
2021-02-07 16:27:59 -0300

    Key props y map()

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -2,14 +2,13 @@ import React from 'react' ;
 
 function Pelicula(props) {
 
-    let generos = ["Accion", "Drama"]
 
     return (
         <>
             <h2>{props.titulo}</h2>
             <p>Rating: {props.rating} </p>
             <ul>
-                {generos.map( genero => <li> {genero} </li>)}
+                {props.generos.map( (genero,i) => <li key={genero+i} > {genero} </li>)}
             </ul>
         </>
     )
--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -3,10 +3,10 @@ import Pelicula from './Pelicula';
 function TiraPeliculas() {
     return (
         <div>
-            <Pelicula rating = '8.0' titulo= 'La delgada linea roja'/>
-            <Pelicula rating = '9.5' titulo = 'Apocalipsis now' />
-            <Pelicula rating = '7.0' titulo = 'Inception'/>
-            <Pelicula rating = '8.5' titulo = 'Nieve negra'/>
+            <Pelicula rating = '8.0' titulo= 'La delgada linea roja'generos ={['accion', 'drama']} />
+            <Pelicula rating = '9.5' titulo = 'Apocalipsis now' generos ={['accion', 'drama']} />
+            <Pelicula rating = '7.0' titulo = 'Inception' generos = {['ciencia ficcion', 'suspenso']} />
+            <Pelicula rating = '8.5' titulo = 'Nieve negra'generos = {['drama']} />
         </div>
     )
 }
