d963df155300b72a279fc306c08fdb8dcc4ec596
Mora-Dav <moradavi@gmail.com>
2021-02-08 09:38:09 -0300

    defaultProps

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -2,18 +2,30 @@ import React from 'react' ;
 
 function Pelicula(props) {
 
+    let listadoDeGeneros;
+
+    if (props.generos != null) {
+        listadoDeGeneros =
+        <ul>
+            {props.generos.map( (genero,i) => <li key={genero+i} > {genero} </li>)}
+        </ul>
+    } else {
+        listadoDeGeneros = ""
+    }
 
     return (
-        <>
+        <div>
             <h2>{props.titulo}</h2>
             <p>Rating: {props.rating} </p>
-            <ul>
-                {props.generos.map( (genero,i) => <li key={genero+i} > {genero} </li>)}
-            </ul>
-        </>
+                { listadoDeGeneros }
+        </div>
     )
 }
 
+Pelicula.defaultProps = {
+    rating : "Esta pelicula aún no fue puntuada!",
+    generos : null
+}
 
 
 export default Pelicula ;
--- src/components/TiraPeliculas.js
+++ src/components/TiraPeliculas.js
@@ -4,9 +4,9 @@ function TiraPeliculas() {
     return (
         <div>
             <Pelicula rating = '8.0' titulo= 'La delgada linea roja'generos ={['accion', 'drama']} />
-            <Pelicula rating = '9.5' titulo = 'Apocalipsis now' generos ={['accion', 'drama']} />
+            <Pelicula titulo = 'Apocalipsis now' generos ={['accion', 'drama']} />
             <Pelicula rating = '7.0' titulo = 'Inception' generos = {['ciencia ficcion', 'suspenso']} />
-            <Pelicula rating = '8.5' titulo = 'Nieve negra'generos = {['drama']} />
+            <Pelicula rating = '8.5' titulo = 'Nieve negra' />
         </div>
     )
 }
